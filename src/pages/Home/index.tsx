a228c99a59b3bd7783c5c7e403af24c6803a4ab8
whyy <icarovsilva1@gmail.com>
2022-12-27 22:32:48 -0300

    feat: create timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import { Play } from 'phosphor-react'
-import { useId } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { useEffect, useId, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { nanoid } from 'nanoid'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   Container,
@@ -13,24 +15,65 @@ import {
 import { newTaskFormValidatorSchema } from '../../schemas'
 
 type IFormProps = z.infer<typeof newTaskFormValidatorSchema>
+interface ICycle {
+  id: string
+  task: string
+  time: number
+  startDate: Date
+}
 
 export function Home() {
   const inputWorkId = useId()
   const inputTimerId = useId()
   const formId = useId()
   const taskListId = useId()
-  const { register, handleSubmit, watch } = useForm<IFormProps>({
+  const { register, handleSubmit, watch, reset } = useForm<IFormProps>({
     resolver: zodResolver(newTaskFormValidatorSchema),
     defaultValues: {
       task: '',
       time: 0,
     },
   })
+  const [cycles, setCycles] = useState<ICycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [secondsPassed, setSecondsPassed] = useState(0)
 
   const isSubmitDisabled = !watch('task')
 
-  const onSubmit: SubmitHandler<IFormProps> = (data) => {
-    console.log(data)
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  const totalSeconds = activeCycle ? activeCycle.time * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - secondsPassed : 0
+
+  const minutes = Math.floor(currentSeconds / 60)
+  const seconds = currentSeconds % 60
+  const minutesFormattedFromInterface = String(minutes).padStart(2, '0')
+  const secondsFormattedFromInterface = String(seconds).padStart(2, '0')
+
+  useEffect(() => {
+    if (!activeCycle) return
+
+    const interval = setInterval(() => {
+      setSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [secondsPassed, activeCycle])
+
+  async function onSubmit({ task, time }: IFormProps) {
+    const id = nanoid()
+
+    const newCycle: ICycle = {
+      id,
+      task,
+      time,
+      startDate: new Date(),
+    }
+    setCycles((prev) => [...prev, newCycle])
+    setActiveCycleId(id)
+    reset()
   }
 
   return (
@@ -60,11 +103,11 @@ export function Home() {
         <span>minutos.</span>
       </form>
       <CountdownContainer>
-        <span>0</span>
-        <span>0</span>
+        <span>{minutesFormattedFromInterface[0]}</span>
+        <span>{minutesFormattedFromInterface[1]}</span>
         <Separator>:</Separator>
-        <span>0</span>
-        <span>0</span>
+        <span>{secondsFormattedFromInterface[0]}</span>
+        <span>{secondsFormattedFromInterface[1]}</span>
       </CountdownContainer>
       <CountdownButton type="submit" form={formId} disabled={isSubmitDisabled}>
         <Play size={24} />
